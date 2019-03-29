4e692420de6dce66a6dd0bc041de82ead420156e
gaoxiaotao <1064152681@qq.com>
2019-03-29 11:37:23 +0800

    接口处理ok

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 /*查询套餐*/
-var URL = 'http://12d30dcd.ngrok.io';
-var PGT = "TGT-5-VRuOHL5pRdNTbYrRDnUU2bhwhDL7zo71ZUODlc0NdX3HwefsMi-cas";
+var URL = 'http://529aa479.ngrok.io';
+var PGT = "TGT-2-EUcLErA4bEpeBtXhWL6VGTdyRFLxszTlZbueIlyQjwcfWrVwak-cas";
+var payTime = 5000; //支付轮询时间
+
 function getRequest(url,callback){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
         if(xhr.readyState == 4 && xhr.status == 200){
-            console.log("success");
-            callback(xhr.responseText,1);
+            callback(xhr.responseText,1);  //传值1不能删除，用于index页面的ajax请求
         }else{
-            console.log("err="+xhr.statusText);
+            //console.log("err="+xhr.statusText);
         }
 
     }
